53dfa708855d7d8a308b61a69c55280e58ea5b76
hotta <g1327061@st.t-kougei.ac.jp>
2016-11-23 21:45:27 +0900

    first commit.

---
+++ app.js
@@ -0,0 +1,12 @@
+var http = require('http');
+//サーバインスタンス作成
+var server = http.createServer();
+var io = require('socket.io').listen(server);
+
+server.listen(8888);//8888番ポートで起動
+
+//接続確立時の処理
+io.sockets.on('connection', function (socket) {
+    // この中でデータのやり取りを行う
+    console.log('connected');
+});
