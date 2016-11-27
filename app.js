fdff68bbef6002a2c56ffd008799212855d04a0e
hotta <g1327061@st.t-kougei.ac.jp>
2016-11-27 20:18:14 +0900

    heroku commit 1127

--- app.js
+++ app.js
@@ -3,10 +3,20 @@ var http = require('http');
 var server = http.createServer();
 var io = require('socket.io').listen(server);
 
-server.listen(process.env.PORT);//8888番ポートで起動
+server.listen(process.env.PORT);
+
+var DemonData = {};
 
 //接続確立時の処理
-io.sockets.on('connection', function (socket) {
+io.sockets.on('connection', function (socket)
+{
     // この中でデータのやり取りを行う
     console.log('connected');
+
+    //送られたデーモンのデータ送信
+    socket.on("DemonPush", function (DemonData)
+    {
+        socket.broadcast.emit("DemonPush", DemonData);
+    });
+
 });
